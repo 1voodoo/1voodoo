03d5cbdb620b790b5f17c2e3dde912a7730c0fab
IHAR <pixel0101@mail.ru>
2022-06-15 19:16:54 +0300

    lesson 02

--- home-programming/pages/page1.js
+++ home-programming/pages/page1.js
@@ -1,1 +1,23 @@
 console.log('I second page');
+const container = document.getElementById('elem');
+const btnAll = document.querySelectorAll('.btn');
+for (const item of btnAll) {
+    item.addEventListener('click', (event) => {
+        if(event) {
+            item.classList.add('btn2')
+        }    
+        for (const item of btnAll) {
+            if(item.className === 'btn btn2') {
+                
+            }
+        }
+        // if (event & item.className === 'btn btn2') {
+        //     item.classList.remove('btn2')
+        // }
+        // item.classList.add('btn2')
+        // if (item.className === 'btn btn2') {
+        //     item.classList.remove('btn2')
+        // }
+        
+    })
+}
